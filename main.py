a4c5b7b3cbe91bbfdf208641260d226357b13b70
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-20 12:23:08 -0400

    adding work

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+url = 'https://www.youtube.com/watch?v=PeOC16IxKwg'
+r = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"})
+html_bytes = r.text
+soup = BeautifulSoup(html_bytes, 'html.parser')
+
+elements = soup.find_all('div', class_='segment')
+
+
+print(elements)
