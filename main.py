9628c3059a0b010f394d720b124f2214dbb3a405
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-23 10:39:44 -0400

    adding TODO

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
+#implement selenium clicking on transcript. get data, parse data. output as a string for input with chatgpt/other AI. 
+
 
 url = 'https://www.youtube.com/watch?v=PeOC16IxKwg'
 r = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"})
